1a6dd34406553f757ffb57dc4dd1b3267d14cbc9
Santiago Romero <santyago.2000@hotmail.com>
2026-03-23 18:39:26 -0500

    Feature: initial commit with deployment files

---
+++ src/main/java/com/artists/artlasapi/ArtlasApiApplication.java
@@ -0,0 +1,13 @@
+package com.artists.artlasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtlasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtlasApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artists/artlasapi/ArtlasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artists.artlasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtlasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

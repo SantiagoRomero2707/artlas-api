a03f97ec201a5d4c6543c587018b3c0c3d42881f
Santiago Romero <santyago.2000@hotmail.com>
2026-03-25 19:44:07 -0500

    Test: refactor tests

--- src/test/java/com/artists/artlasapi/repository/ArtistRepositoryIT.java
+++ src/test/java/com/artists/artlasapi/repository/ArtistRepositoryIT.java
@@ -1,6 +1,7 @@
 package com.artists.artlasapi.repository;
 
 import com.artists.artlasapi.entity.Artist;
+import com.artists.artlasapi.utils.ArtistTestUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -33,22 +34,16 @@ public class ArtistRepositoryIT {
     @Test
     @Rollback(value = false)
     @Order(1)
-    public void testSaveArtist(){
-        Artist artistEntity = new Artist.ArtistBuilder()
-                .setName("Johan Santiago")
-                .setLastName("Romero Duarte")
-                .setTypeIDE("AB-CD")
-                .setNumberIDE("EF-GH")
-                .build();
-
-        Artist artistSaved = artistEntityRepository.save(artistEntity);
+    public void testSaveArtist() {
+        Artist artistSaved = artistEntityRepository.save(ArtistTestUtils.buildArtist());
         idArtist = artistSaved.getIdArtists();
         assertNotNull(artistSaved);
+        assertTrue(artistSaved.getIdArtists() > 0);
     }
 
     @Test
     @Order(2)
-    public void testFindById(){
+    public void testFindById() {
         Optional<Artist> artistEntity = artistEntityRepository.findById(idArtist);
         assertTrue(artistEntity.isPresent());
         assertEquals(idArtist, artistEntity.get().getIdArtists());
@@ -57,50 +52,34 @@ public class ArtistRepositoryIT {
     @Test
     @Rollback(value = false)
     @Order(3)
-    public void testUpdateArtist(){
+    public void testUpdateArtist() {
         String newLastName = "Sabogal Castro";
 
         Optional<Artist> existArtist = artistEntityRepository.findById(idArtist);
-        if(existArtist.isPresent()){
-            Artist artistEntity = new Artist.ArtistBuilder()
-                    .setIdArtists(idArtist)
-                    .setName(existArtist.get().getName())
-                    .setLastName(newLastName)
-                    .setTypeIDE(existArtist.get().getTypeIDE())
-                    .setNumberIDE(existArtist.get().getNumberIDE())
-                    .build();
+        assertTrue(existArtist.isPresent(), "El artista no está creado en base de datos");
 
-            artistEntityRepository.save(artistEntity);
-        }
+        existArtist.get().setLastName(newLastName);
+        artistEntityRepository.save(existArtist.get());
 
-        Optional<Artist> updateArtist = artistEntityRepository.findById(idArtist);
-        if(updateArtist.isEmpty()){
-            throw new RuntimeException("El artista no está creado en base de datos");
-        }
-        assertEquals(newLastName, updateArtist.get().getLastName());
+        Optional<Artist> updatedArtist = artistEntityRepository.findById(idArtist);
+        assertTrue(updatedArtist.isPresent());
+        assertEquals(newLastName, updatedArtist.get().getLastName());
     }
 
     @Test
     @Order(4)
-    public void testListArtist(){
+    public void testListArtist() {
         List<Artist> allArtist = artistEntityRepository.findAll();
-
-        for(Artist artistEntity: allArtist){
-            System.out.println(artistEntity);
-        }
+        allArtist.forEach(artist -> log.info("Artist: {}", artist));
         assertFalse(allArtist.isEmpty());
     }
 
     @Test
     @Rollback(value = false)
     @Order(5)
-    public void testDeleteArtist(){
-        boolean isExistBeforeDelete = artistEntityRepository.findById(idArtist).isPresent();
+    public void testDeleteArtist() {
+        assertTrue(artistEntityRepository.findById(idArtist).isPresent());
         artistEntityRepository.deleteById(idArtist);
-
-        boolean notExistAfterDelete = artistEntityRepository.findById(idArtist).isPresent();
-
-        assertTrue(isExistBeforeDelete);
-        assertFalse(notExistAfterDelete);
+        assertFalse(artistEntityRepository.findById(idArtist).isPresent());
     }
 }

c68a05870d9fea9d152e500985395fda73d6174e
Santiago Romero <santyago.2000@hotmail.com>
2026-04-17 17:47:30 -0500

    Feat: add redis config

---
+++ src/main/java/com/artists/artlasapi/configuration/RedisConfig.java
@@ -0,0 +1,9 @@
+package com.artists.artlasapi.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+}
--- src/main/java/com/artists/artlasapi/configuration/SwaggerConfig.java
+++ src/main/java/com/artists/artlasapi/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.artists.artlasapi.util;
+package com.artists.artlasapi.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -10,8 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info =@Info(title = "ARTLAS API", version = "1.0.0", description ="Java Backend proof of concept") )
+@OpenAPIDefinition(info = @Info(title = "ARTLAS API", version = "1.0.0", description ="Java Backend proof of concept") )
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
